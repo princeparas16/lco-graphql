dafd6927f460fc410226fcb9a3fbf181e775ff65
princeparas16 <princeparas16@gmail.com>
2021-05-25 12:49:43 +0530

    Intial stage completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Start your es6 scripts here
+import express from "express";
+import resolvers from "./resolvers";
+import schema from "./schema";
+import { graphqlHTTP } from "express-graphql";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Up and running with graphQL");
+});
+
+const root = resolvers;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(8080, () => console.log("Running at port 8080"));
